f82a826359c8917930356624f4489bacb420a382
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-13 22:00:47 +0530

    refector

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,45 +1,47 @@
-import React from "react"
-import { Router, Route, Switch } from "react-router-dom"
-import createBrowserHistory from "./history"
-import Header from "./Header"
-import Footer from './Footer'
-import CompA from "./CompA"
-import CompB from "./CompB"
-import CompC from "./CompC"
-import CompD from "./CompD"
-import RegisterComponent from './RegisterComponent'
-import Forms from './Forms'
-import Login from './Login'
-import NotFound from "./NotFound"
-import Favourite from "./Favourite"
-import UserData from './UserData'
+/** @format */
+
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createBrowserHistory from './history';
+import Header from './Header';
+import Footer from './Footer';
+import CompA from './CompA';
+import CompB from './CompB';
+import CompC from './CompC';
+import CompD from './CompD';
+import RegisterComponent from './RegisterComponent';
+import Forms from './Forms';
+import Login from './Login';
+import NotFound from './NotFound';
+import Favourite from './Favourite';
+import UserData from './UserData';
 const App = () => {
-  return (
-    <div className="App">
-      <Router history={createBrowserHistory}>
-        <header>
-          <Header />
-        </header>
-        <article>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={RegisterComponent} />
-            
-            <Route exact path="/register1" component={Forms} />
-            <Route path="/home/:data" component={CompA} />
-            <Route path="/list" component={CompB} />
-            <Route path="/shop/:id" component={CompC} />
-            <Route path="/detail/:id" component={CompD} />
-            <Route path="/fav" component={Favourite} />
-            <Route path="/userdata" component={UserData} />
-            <Route path="*" exact component={NotFound} />
-          </Switch>
-        </article>
-        <footer>
-          <Footer />
-        </footer>
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router history={createBrowserHistory}>
+				<header>
+					<Header />
+				</header>
+				<article>
+					<Switch>
+						<Route exact path='/' component={Login} />
+						<Route exact path='/register' component={RegisterComponent} />
+
+						<Route exact path='/register1' component={Forms} />
+						<Route path='/home/:data' component={CompA} />
+						<Route path='/list' component={CompB} />
+						<Route path='/shop/:id' component={CompC} />
+						<Route path='/detail/:id' component={CompD} />
+						<Route path='/fav' component={Favourite} />
+						<Route path='/userdata' component={UserData} />
+						<Route path='*' exact component={NotFound} />
+					</Switch>
+				</article>
+				<footer>
+					<Footer />
+				</footer>
+			</Router>
+		</div>
+	);
 };
 export default App;
